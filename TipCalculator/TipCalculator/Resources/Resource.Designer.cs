e1788e9a769ff8f72612a6319e620b863ffe4f13
FreelanceComputing <FreelanceComputing@users.noreply.github.com>
2018-01-30 20:43:44 +0000

    added views to the axml layout using the designer view

--- TipCalculator/TipCalculator/Resources/Resource.Designer.cs
+++ TipCalculator/TipCalculator/Resources/Resource.Designer.cs
@@ -57,6 +57,43 @@ namespace TipCalculator
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int billAmountEdit = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int calculateBtn = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int linearLayout1 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int tipValView = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int tipView = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int totalValView = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int totalView = 2131034118;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
