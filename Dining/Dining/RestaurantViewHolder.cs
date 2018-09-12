134ed924c0dde83636db3e54d2979506821c864d
freelance <freelance-computing@outlook.com>
2018-09-12 20:19:53 +0100

    extracted restaurant view holder to a separate file

---
+++ Dining/Dining/RestaurantViewHolder.cs
@@ -0,0 +1,33 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using System;
+
+namespace Dining
+{
+    public class RestaurantViewHolder : RecyclerView.ViewHolder
+    {
+        public RestaurantViewHolder(View itemView, Action<int> clickListener)
+            : base(itemView)
+        {
+            Image = itemView.FindViewById<ImageView>(Resource.Id.imageView);
+            Rating = itemView.FindViewById<RatingBar>(Resource.Id.ratingBar);
+            Name = itemView.FindViewById<TextView>(Resource.Id.nameTextView);
+
+            itemView.Click += (s, e) =>
+            {
+                int position = base.AdapterPosition;
+                if (position == RecyclerView.NoPosition)
+                    return;
+                clickListener(position);
+            };
+        }
+
+        public ImageView Image { get; private set; }
+
+        public RatingBar Rating { get; private set; }
+
+        public TextView Name { get; private set; }
+    }
+}
+
